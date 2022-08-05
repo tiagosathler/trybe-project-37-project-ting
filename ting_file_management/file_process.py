db6e9b61bffd813a4567706a279d6ce526f190cd
Tiago <sathler@gmail.com>
2022-08-05 15:11:11 -0300

    :sparkles: feat[file_process.py]: implementa a função 'process'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,24 @@
-def process(path_file, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
+from ting_file_management.file_management import txt_importer
+
+
+def process(path_file: str, instance: Queue) -> None:
+    if (len(instance) > 0):
+        for index in range(len(instance)):
+            data = instance.search(index)
+            if data["nome_do_arquivo"] == path_file:
+                return None
+
+    file_content = txt_importer(path_file)
+
+    data = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(file_content),
+        "linhas_do_arquivo": file_content,
+    }
+
+    instance.enqueue(data)
+    print(data)
 
 
 def remove(instance):
