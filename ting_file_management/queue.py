6508995c8b8e5cfed8e79302209ac25cf4db8063
Tiago <sathler@gmail.com>
2022-08-05 15:13:51 -0300

    :recycle: refactor[queue.py]: altera o tipo de 'value' para 'dict'

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -8,13 +8,13 @@ class Queue:
     def __len__(self) -> int:
         return len(self._data)
 
-    def enqueue(self, value: int) -> None:
+    def enqueue(self, value: dict) -> None:
         self._data.append(value)
 
     def dequeue(self) -> int:
         return self._data.popleft()
 
-    def search(self, index: int) -> int:
+    def search(self, index: int) -> dict:
         if (index < 0 or index > len(self)):
             raise IndexError("Index out of bounds")
         else:
