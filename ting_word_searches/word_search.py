bfc079e153f53e4c8628645cedcd4056404f657d
Tiago <sathler@gmail.com>
2022-08-05 16:16:13 -0300

    :sparkles: feat[word_search.py]: implementa as funções 'search_by_word' e 'exists_word'

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,37 @@
-def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+from ting_file_management.queue import Queue
 
 
-def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+def exists_word(word: str, instance: Queue):
+    response = search_by_word(word, instance)
+    if len(response):
+        response = [{
+            "palavra": element["palavra"],
+            "arquivo": element["arquivo"],
+            "ocorrencias": [{
+                "linha": occurrence["linha"]
+            } for occurrence in element["ocorrencias"]]
+        } for element in response]
+    return response
+
+
+def search_by_word(word: str, instance: Queue) -> list:
+    result = []
+    for index in range(len(instance)):
+        occurrences = []
+        meta_data = instance.search(index)
+        for line, line_content in enumerate(meta_data["linhas_do_arquivo"]):
+            if (word.lower() in line_content.lower()):
+                occurrence = {
+                    "linha": line + 1,
+                    "conteudo": line_content
+                }
+                occurrences.append(occurrence)
+        if len(occurrences):
+            data = {
+                "palavra": word,
+                "arquivo": meta_data["nome_do_arquivo"],
+                "ocorrencias": occurrences,
+            }
+            result.append(data)
+
+    return result
