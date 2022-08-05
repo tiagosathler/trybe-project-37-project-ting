57aff25ae10f17f7922dcf6407ae359445ef180d
Tiago <sathler@gmail.com>
2022-08-05 14:32:46 -0300

    :sparkles: feat[queue.py]: implementa a classe Queue

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -1,15 +1,21 @@
+from collections import deque
+
+
 class Queue:
-    def __init__(self):
-        """Inicialize sua estrutura aqui"""
+    def __init__(self) -> None:
+        self._data = deque()
 
-    def __len__(self):
-        """Aqui irá sua implementação"""
+    def __len__(self) -> int:
+        return len(self._data)
 
-    def enqueue(self, value):
-        """Aqui irá sua implementação"""
+    def enqueue(self, value: int) -> None:
+        self._data.append(value)
 
-    def dequeue(self):
-        """Aqui irá sua implementação"""
+    def dequeue(self) -> int:
+        return self._data.popleft()
 
-    def search(self, index):
-        """Aqui irá sua implementação"""
+    def search(self, index: int) -> int:
+        if (index < 0 or index > len(self)):
+            raise IndexError("Index out of bounds")
+        else:
+            return self._data[index]
