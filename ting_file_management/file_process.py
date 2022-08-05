35766d95b282463851e1be46426d67f029dc19db
Tiago <sathler@gmail.com>
2022-08-05 15:20:12 -0300

    :sparkles: feat[file_process.py]: implementa a função 'remove'

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,6 @@
 from ting_file_management.queue import Queue
 from ting_file_management.file_management import txt_importer
+import sys
 
 
 def process(path_file: str, instance: Queue) -> None:
@@ -21,8 +22,13 @@ def process(path_file: str, instance: Queue) -> None:
     print(data)
 
 
-def remove(instance):
-    """Aqui irá sua implementação"""
+def remove(instance: Queue) -> None:
+    if not len(instance):
+        print("Não há elementos", file=sys.stdout)
+
+    else:
+        instance.dequeue()
+        print("Arquivo statics/arquivo_teste.txt removido com sucesso")
 
 
 def file_metadata(instance, position):
