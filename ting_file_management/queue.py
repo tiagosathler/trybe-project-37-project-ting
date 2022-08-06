3800b3b6dde55d99e212686ffdb6c94b66f495d1
Tiago <sathler@gmail.com>
2022-08-06 15:22:15 -0300

    :books: docs[queue.py]: comenta a classe Queue e seus métodos

--- ting_file_management/queue.py
+++ ting_file_management/queue.py
@@ -3,26 +3,60 @@ from copy import deepcopy
 
 
 class Queue:
+    """
+    Classe que representa a Estrutura de Dados de uma Fila.
+
+    Utiliza a lista bidirecional 'deque' da biblioteca 'collections'
+    do Python para implementar a Fila de forma performática. "Deque"
+    é a abreviação para "double-ended-queue", ou seja, "fila duplamente
+    terminada". É usada de forma segura e eficiente para implementar
+    tanto "Pilhas" quanto "Filas".
+    (https://docs.python.org/pt-br/3/library/collections.html#collections.deque)
+
+    A Fila é uma estrutura de dados que o utiliza o princípio
+    do "First In, First Out" (FIFO).
+    """
     def __init__(self) -> None:
         self._data = deque()
 
     def __len__(self) -> int:
+        """
+        Representação do tamanho da Fila.
+        """
         return len(self._data)
 
     def is_empty(self) -> bool:
+        """
+        Método que retorna o booleano se a Fila está vazia.
+        """
         return not bool(self._data)
 
     def enqueue(self, value: dict) -> None:
+        """
+        Método que enfileira um dicionário à Fila (primeira posição do deque).
+        """
         self._data.append(value)
 
     def dequeue(self) -> dict:
+        """
+        Método que desenfileira o primeiro dicionário da Fila.
+        Retorna o dicionário
+        """
         return self._data.popleft()
 
     def search(self, index: int) -> dict:
+        """
+        Método que busca o dicionário na posição 'index' da Fila.
+        Retorna o dicionário
+        """
         if (0 <= index < len(self)):
             return self._data[index]
         else:
             raise IndexError("Index out of bounds")
 
     def copy(self):
+        """
+        Método que faz uma cópia profunda e independente da instância.
+        Retorna a cópia desta instância de Queue.
+        """
         return deepcopy(self)
